6cc66e92eb6d799ae1570ce3236b5e46cbfc275d
Alec Keeler <alec.keeler@gmail.com>
2023-06-12 16:09:55 -0500

    enum testing

---
+++ our_demo/poke_api/db/seeders/20230612210613-test-stuff.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   const {Test} = require('../models')
+   await Test.bulkCreate([
+    {stuff: 'thing1'},
+    {stuff: 'thing2'},
+    // {stuff: 'thing3'}, //errors out appropriately with this record commented in
+   ], {validate: true})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
